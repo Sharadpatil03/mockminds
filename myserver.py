653c644e1b6103c3af6122e25169617b9a8394be
rutu1717 <kruturaj96@gmail.com>
2025-01-27 12:21:34 +0530

    latest commit

--- myserver.py
+++ myserver.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from langchain.evaluation import load_evaluator
 from langchain_groq import ChatGroq
 from typing import List
 
 app = FastAPI()
+
+# CORS setup to allow all
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,  # Allow cookies to be sent
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 criteria_completeness = {
     "completeness": """
